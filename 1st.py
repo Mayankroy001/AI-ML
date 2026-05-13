22e6735f9b98af461940b04308aed64e70c532c2
Mayankroy001 <157673205+Mayankroy001@users.noreply.github.com>
2026-05-13 23:46:43 +0530

    Arithmatic addition

--- 1st.py
+++ 1st.py
@@ -5,4 +5,19 @@ print(Name)
 
 st = "1235286i980eipdwjlkn! c"
 print(st)
-print(type(st))
+# print(type(st))
+
+
+num = int(input("enter your number : "))
+print(type(f"your number is {num}"))
+print(f"your number is {num}")
+
+
+
+a = 10
+b = 20
+print("The sum : ", a + b)
+
+
+
+
