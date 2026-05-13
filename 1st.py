41deb2789595a80c611b76756391e5ae5e4f9335
Mayankroy001 <157673205+Mayankroy001@users.noreply.github.com>
2026-05-14 00:09:58 +0530

    comparison operators

--- 1st.py
+++ 1st.py
@@ -25,4 +25,14 @@ print("The power : ", a ** b) #this type of operation as a^b is called power ope
 
 
 
+#comaprison operators
 
+x = 10
+y = 20
+
+print(x==y) #equal to
+print(x!=y) #not equal to
+print(x>y) #greater than
+print(x<y) #less than
+print(x>=y) #greater than or equal to
+print(x<=y) #less than or equal to
