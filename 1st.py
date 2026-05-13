3e3e237350ab80355bb834672b82dccd57f213a8
Mayankroy001 <157673205+Mayankroy001@users.noreply.github.com>
2026-05-13 23:52:41 +0530

    Arithmatic operation

--- 1st.py
+++ 1st.py
@@ -13,10 +13,15 @@ print(type(f"your number is {num}"))
 print(f"your number is {num}")
 
 
-
+#Arithmetic Operators
 a = 10
 b = 20
 print("The sum : ", a + b)
+print("The sub : ", a - b)
+print("The product : ", a * b)
+print("The quotient : ", a / b)
+print("The floor quotient : ", a // b)
+print("The power : ", a ** b) #this type of operation as a^b is called power operation
 
 
 
