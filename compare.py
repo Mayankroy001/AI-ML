e1b0a03db5a727e02568b4238908884c9b8cbaaf
Mayankroy001 <157673205+Mayankroy001@users.noreply.github.com>
2026-05-15 23:54:36 +0530

    comparison operators ORD

--- compare.py
+++ compare.py
@@ -7,3 +7,10 @@ print(a>b)
 print(a<b)
 print(a>=b)
 print(a<=b)
+
+
+print(ord('A')) #ord(ascii value of a string) function gives the unicode of the character
+print(ord('B'))
+
+print("A" > "B") #it compares the unicode of the characters and gives the result accordingly
+print("A" < "B")
