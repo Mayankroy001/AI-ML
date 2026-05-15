0180200b0b1a6a81502eac64304ba3b00d0e2767
Mayankroy001 <157673205+Mayankroy001@users.noreply.github.com>
2026-05-16 00:21:23 +0530

    logical operators

--- compare.py
+++ compare.py
@@ -14,3 +14,7 @@ print(ord('B'))
 
 print("A" > "B") #it compares the unicode of the characters and gives the result accordingly
 print("A" < "B")
+
+print(123>100 and 123==123 and 123<100)#logical and operator = agar dono condition true hai to hi true return karega nehi to false return karega
+print(123>100 or 123==123 or 123<100) #logical or operator = agar dono condition me se koi bhi ek condition true hai to true return karega nehi to false return karega
+print(not 12==12) #logical not operator = agar condition true hai to false return karega aur agar condition false hai to true return karega
